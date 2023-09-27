1cf34be4106bd58a2de488ba3ed6d1961f90f2ac
Anora <anoranarimanovna@gmail.com>
2023-09-27 16:04:51 +0500

    added admin.py and apps.py and models.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,37 +1,58 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
 
-@admin.register(Course)
-class CourseAdmin(admin.ModelAdmin):
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
     fields = (
-        'title',
-        'about',
-        'price',
-        'duration',
+        'name',
         'image',
+        'level',
+        'about',
         'phone',
     )
 
-    list_display = ['title','about','price','duration','image','phone']
-    
+    list_display = ['name', 'image', 'level', 'about', 'phone']
 
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    fields = (
+        'picture',
+        'title', 
+        'about',
+        'vidio',
+    )
 
-admin.site.register(Contact)
+    list_display = ['picture', 'title', 'about', 'vidio']
 
+@admin.register(Students)
+class StudentsAdmin(admin.ModelAdmin):
+    fields = (
+        'fullname',
+        'email',
+        'number',
+        'extnumber',
+        'day',
+        'stu_day',
+    ) 
+
+    list_display = ['fullname', 'email', 'number', 'extnumber', 'day', 'stu_day']
 
 @admin.register(Level)
 class LevelAdmin(admin.ModelAdmin):
     fields = (
-        'image',
-        'title',
-        'reading',
-        'listening',
-        'writing',
-        'speaking',
+        'lider',
     )     
 
-    list_display = ['image', 'title', 'reading', 'listening', 'writing', 'speaking']
+    list_display = ['lider']
 
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    fields = (
+        'tel_nomer',
+        'email',
+        'level',
+        'message',
+    )    
 
+    list_display = ['tel_nomer', 'email', 'level', 'message']
